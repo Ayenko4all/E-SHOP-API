bc08bcb39c31a4c43326a0919ad658fc3fe0fc3a
Ayeni kehinde Oluwatosin <kenny4all40@gmail.com>
2024-05-01 23:37:50 +0100

    added file upload function

---
+++ api/helpers/fileUpload.ts
@@ -0,0 +1,26 @@
+import multer, { diskStorage } from "multer";
+
+const storage = diskStorage({
+  destination: function (req: any, file: any, cb: any) {
+    cb(null, "uploads/");
+  },
+  filename: function (req: any, file: any, cb: any) {
+    cb(null, new Date().toISOString().slice(0, 10) + file.originalname);
+  },
+});
+
+const fileFilter = (req: any, file: any, cb: any) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+  fileFilter: fileFilter,
+});
+
+export default upload;
--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
+import logger from "morgan";
 import bodyParser from "body-parser";
 import path from "path";
 import dotenv from "dotenv";
@@ -9,16 +10,13 @@ import routes from "./api/routes/index";
 
 const { urlencoded, json } = bodyParser;
 const { connect } = mongoose;
-
 const app = express();
 
-const BaseUrl: any = process.env.MONGO_URL;
-
+app.use(logger("dev"));
 app.use(urlencoded({ extended: false }));
 app.use(json());
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use(bodyParser.json());
+app.use("uploads", express.static("uploads"));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -52,6 +50,8 @@ app.use((error: any, req: Request, res: Response, next: NextFunction) => {
 app.use("/v1", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({ message: "Welcome to typescript and Node js Api" });
 });
+
+const BaseUrl: any = process.env.MONGO_URL;
 
 connect(BaseUrl);
 
